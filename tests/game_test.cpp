2741945b94b9cb8a8d4d6333326e2910d0db66c3
Pranav Shridhar <pranavshridhar0@gmail.com>
2025-06-05 19:04:16 -0500

    Enhance MoveCountTest with move validation

--- tests/game_test.cpp
+++ tests/game_test.cpp
@@ -9,5 +9,14 @@ TEST(GameTest, SizeTest) {
 }
 
 TEST(GameTest, MoveCountTest) {
-	GameBoard gb(7, 10, 1746386351);
+	Game g(7, 10, 1746386351);
+	vector<pair<int, int>> test_moves {
+		{0, 0}, {1, 5}, {2, 2}, {2, 4}, {6, 4}, {6, 2}, {5, 7}
+	};
+
+	for (const auto& move : test_moves) {
+		EXPECT_TRUE(g.make_move(move.first, move.second));
+		g.apply_gravity();
+	}
+	EXPECT_EQ(test_moves.size(), g.moves());
 }
