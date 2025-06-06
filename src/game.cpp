be55057c0cd15a638781617187fc7e641bb6fa8f
Pranav Shridhar <37971130+pranavshridhar@users.noreply.github.com>
2025-06-05 19:06:04 -0500

    Came back from trip (#4)
    * More spaces to tab conversion lol
    
    * Added some comments to Game class
    
    * Enhance MoveCountTest with move validation

--- src/game.cpp
+++ src/game.cpp
@@ -3,20 +3,21 @@
 
 bool Game::make_move(int i, int j) {
   if (!gameboard.validMove(i, j)) {
-    return false;
-  }
-  gameboard.popBubble(i, j);
-  return true;
+		return false;
+	}
+	gameboard.popBubble(i, j);
+	_moves++;
+	return true;
 }
 
 void Game::console_dump() {
-  int rows = gameboard.rows();
-  int columns = gameboard.columns();
+	int rows = gameboard.rows();
+	int columns = gameboard.columns();
 
-  for (int r = 0; r < rows; r++) {
-    for (int c = 0; c < columns; c++) {
-      cout << gameboard(r, c) << " ";
-    }
-    cout << endl;
-  }
+	for (int r = 0; r < rows; r++) {
+		for (int c = 0; c < columns; c++) {
+			cout << gameboard(r, c) << " ";
+		}
+		cout << endl;
+	}
 }
--- tests/game_test.cpp
+++ tests/game_test.cpp
@@ -3,7 +3,20 @@
 
 
 TEST(GameTest, SizeTest) {
-    Game gb(7, 10);
-    EXPECT_EQ(7, gb.rows());
-    EXPECT_EQ(10, gb.columns());
+	Game gb(7, 10);
+	EXPECT_EQ(7, gb.rows());
+	EXPECT_EQ(10, gb.columns());
+}
+
+TEST(GameTest, MoveCountTest) {
+	Game g(7, 10, 1746386351);
+	vector<pair<int, int>> test_moves {
+		{0, 0}, {1, 5}, {2, 2}, {2, 4}, {6, 4}, {6, 2}, {5, 7}
+	};
+
+	for (const auto& move : test_moves) {
+		EXPECT_TRUE(g.make_move(move.first, move.second));
+		g.apply_gravity();
+	}
+	EXPECT_EQ(test_moves.size(), g.moves());
 }
