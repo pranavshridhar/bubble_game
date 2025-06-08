286365ec68b7bfbda173a5642825019aa1680267
Pranav Shridhar <pranavshridhar0@gmail.com>
2025-06-07 21:31:12 -0500

    Refactor random number generation for consistent bubble initialization

--- src/gameboard.cpp
+++ src/gameboard.cpp
@@ -3,6 +3,7 @@
 #include <ctime>
 #include <algorithm>
 #include <numeric>
+#include <random>
 
 #include "gameboard.hpp"
 #include "utilities/out_of_bounds.hpp"
@@ -46,11 +47,13 @@ GameBoard::GameBoard(int rows, int cols)
 
 GameBoard::GameBoard(int rows, int cols, unsigned int seed) : _rows(rows), _columns(cols) {
 	pop_criteria = 3; // FIXME: Just for starters. Change later on.
-	srand(seed);
 	board = vector<vector<int>>(rows, vector<int>(cols));
+
+	mt19937 mt(seed); // RNG for consistency across platforms
+
 	for(vector<int>& row : board) {
 		for(int& element : row) {
-			element = (rand() % 4) + 1;
+			element = (mt() % 4) + 1;
 		}
 	}
 }
